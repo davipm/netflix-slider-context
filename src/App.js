7ce53c4b7804b4ba8a0153c17a8c628114e366ef
davip <davi.p.m94@gmail.com>
2019-08-17 14:56:41 -0300

    add slider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from "./components/NetflixSlider";
 
 function App() {
+  const movies = [
+    {
+      id: 1,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: '1983'
+    },
+    {
+      id: 2,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: 'Russian doll'
+    },
+    {
+      id: 3,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: 'The rain',
+    },
+    {
+      id: 4,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: 'Sex education'
+    },
+    {
+      id: 5,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: 'Elite'
+    },
+    {
+      id: 6,
+      image: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      imageBg: 'https://i.ytimg.com/vi/5qEUC1iE1fI/maxresdefault.jpg',
+      title: 'Black mirror'
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider>
+        {movies.map(movie => (
+          <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+        ))}
+      </Slider>
+      <Slider>
+        {movies.map(movie => (
+          <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+        ))}
+      </Slider>
     </div>
   );
 }
